824c1ea3b60fd1592b0a321b9d12a988d47a5758
dichor94 <dichor@alumnatflorida.es>
2024-09-20 17:40:08 +0200

    Añado el primer cambio en el index.php después del fork

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-echo "¡Hola bienvenidos a este repositorio de prueba!"
+echo "¡Hola bienvenidos a este repositorio de prueba!";
+
+echo "Este mensaje es completamente nuevo, después de hacer un FORK del repositorio de mi compañera!!";
 
 ?>
